701279fded853e6a75bd594d52cc12c7ea3c3aff
losfair <zhy20000919@hotmail.com>
2017-10-28 22:00:10 +0800

    Export modules

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,10 @@ extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
 
-mod engine;
-mod jit;
-mod signals;
-mod var;
+pub mod engine;
+pub mod jit;
+pub mod signals;
+pub mod var;
 
 #[cfg(test)]
 mod engine_test;
